78b0fa81e45eb2e868ebe0226db8be86e3c70e6f
Piers Chambers <piers@varyonic.com>
2022-02-14 14:10:22 -0500

    bundle gem active_merchant-redsys_rest

---
+++ lib/active_merchant/redsys_rest.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "redsys_rest/version"
+
+module ActiveMerchant
+  module RedsysRest
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/active_merchant/redsys_rest/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module ActiveMerchant
+  module RedsysRest
+    VERSION = "0.1.0"
+  end
+end
